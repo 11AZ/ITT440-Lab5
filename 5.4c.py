d879de3efd2231f129ab16e9d447e6ff530dc409
11AZ <hazwanhaz2010@gmail.com>
2020-12-06 14:39:40 +0000

    Client done

--- 5.4c.py
+++ 5.4c.py
@@ -4,11 +4,12 @@ import os
 import json
 import sys
 
+
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096 #send 4096 bytes each time step
 
 #create client socket
-s= socket.socket()
+s = socket.socket()
 #the ip address of the server
 host = "192.168.0.101"
 #port use
@@ -21,27 +22,27 @@ print("[+] COnnected")
 
 #user input file name
 filename = input("Enter file name")
-
+print("Filename:", filename)
 #get the file size
-file size = os.path.getsize(filename)
+filesize = os.path.getsize(filename)
 
 #send the filename and filesize
 s.send(f"{filename}{SEPARATOR}{filesize}".encode())
 
-progress = tqdm.tqdm(range(filesize),f"Sending{filename}", unit ="B", unit_scale=true, unit_divisor=1024)
-with open(filename,"rb")as f:
-
-	for _ in progress:
-	#read the bytes from the file
-	bytes_read= f.read(BUFFER_SIZE)
-	if not bytes_read:
-		#file transmitting is done
-		break
-	#use sendall to assure transimission in busy network
-	s.sendall(bytes_read)
-	#update the progress bar
-	progess.update(len(bytes_read))
-
+#start sending file
+progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(filename, "rb") as f:
+        for _ in progress:
+            # read the bytes from the file
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                # file transmitting is done
+                break
+            # we use sendall to assure transimission in 
+            # busy networks
+            s.sendall(bytes_read)
+            # update the progress bar
+            progress.update(len(bytes_read))
 #close socket
 s.close()
 
